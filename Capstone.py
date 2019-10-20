3a4e2232b06fef67576565f632a2ae667b002f40
varadhan100 <45936889+varadhan100@users.noreply.github.com>
2019-10-20 22:59:57 +0530

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+## #This is the project for Data Science capstone project
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+print (' Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
